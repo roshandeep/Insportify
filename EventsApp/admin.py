88e4d6edd3d714c877db35f46bc606c787821039
Roshandeep Singh Saini <roshandeep810@gmail.com>
2022-05-17 18:18:25 -0400

    Unwanted models deleted, js changes sports info multiple data store

--- EventsApp/admin.py
+++ EventsApp/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
-from EventsApp.models import master_table, User, IsEventTypeMaster, IsEventsDetails, \
-    IsEventsNotification, IsEventsScheduler, IsNotificationMaster, \
+from EventsApp.models import master_table, User, \
     Individual, Organization, SportsCategory, SportsType, Venues, Availability, Order, Logo, Extra_Loctaions, \
     Events_PositionInfo
 
 admin.site.register(master_table)
 admin.site.register(User)
-admin.site.register(IsEventTypeMaster)
-admin.site.register(IsEventsDetails)
-admin.site.register(IsEventsNotification)
-admin.site.register(IsEventsScheduler)
-admin.site.register(IsNotificationMaster)
 admin.site.register(SportsCategory)
 admin.site.register(SportsType)
 admin.site.register(Individual)
