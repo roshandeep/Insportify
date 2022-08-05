ee1e21eaa389888daa178f4d1235fb95b611747b
Roshandeep Singh Saini <roshandeep810@gmail.com>
2022-08-04 20:00:41 -0400

    Update App password for gmail

--- Insportify/settings.py
+++ Insportify/settings.py
@@ -82,7 +82,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'insportifyinfo@gmail.com'
-EMAIL_HOST_PASSWORD = 'xgtfkxdopmidjlhk'
+EMAIL_HOST_PASSWORD = 'vgqwluqxsyrcdqwd'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
