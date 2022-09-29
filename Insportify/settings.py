6b80bc094e6313ef6d968e2da7b7252e919ff256
Roshandeep Singh Saini <roshandeep810@gmail.com>
2022-09-29 14:30:35 -0400

    All urls in allowed host change

--- Insportify/settings.py
+++ Insportify/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ck(g2(+&@t-o2-mssd@xbx4@+offxrv1o-c&2u-lvts2&1%g+w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.182.44.234', '127.0.0.1', 'localhost', 'insportify.com:8000', 'www.insportify.com', '*']
+ALLOWED_HOSTS = ['35.182.44.234', '127.0.0.1', 'localhost', 'insportify.com:8000', 'www.insportify.com', '*.insportify.*']
 
 # Application definition
 
