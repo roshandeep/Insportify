b45591280113a0c4c8cfc21842d53d3ba646bcb4
Roshandeep Singh Saini <roshandeep810@gmail.com>
2022-10-18 20:23:43 -0400

    static file changes

--- Insportify/urls.py
+++ Insportify/urls.py
@@ -19,8 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('EventsApp.urls')),
-    path('users/', include('UserRegister.urls')),
-    # path('users/', include('django.contrib.auth.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('EventsApp.urls')),
+                  path('users/', include('UserRegister.urls')),
+                  # path('users/', include('django.contrib.auth.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
+                                                                                         document_root=settings.STATIC_ROOT)
