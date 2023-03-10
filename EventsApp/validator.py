67a3675d23400a257f8701326c023a9f708be309
Roshandeep Singh Saini <roshandeep810@gmail.com>
2023-03-09 22:46:47 -0500

    Ad Image size attribute print

--- EventsApp/validator.py
+++ EventsApp/validator.py
@@ -1,9 +1,9 @@
 from django.core.exceptions import ValidationError
+from django.core.files.images import get_image_dimensions
 
 
-def image_restriction(value):
-    print(value)
-    image_width = 1600
-    image_height = 1600
+def image_restriction(image):
+    image_width, image_height = get_image_dimensions(image)
+    # print(image_width, image_height)
     if image_width >= 360 or image_height >= 140:
         raise ValidationError('Image width needs to be less than 360px * 140px')
